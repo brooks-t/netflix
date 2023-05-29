5d04eb60243b6a35f23377d5ed61ab9c793ee6f5
Brooks Tiffany <brooks.t.tiffany@gmail.com>
2023-05-29 13:53:28 -0400

    added styling to rows and banners

--- src/Row.js
+++ src/Row.js
@@ -1,45 +1,46 @@
-import React, { useState, useEffect } from 'react';
-import axios from './axios';
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
 // axios is the 'instance' we created in axios.js. since you exported it as a default, you can name it whatever you want when you import it. but if you didn't export it as a default, you would have to import it like this: import { instance } from './axios';
+import "./Row.css";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-function Row({ title, fetchURL }) {
-    const [movies, setMovies] = useState([]);
+function Row({ title, fetchURL, isLargeRow }) {
+	const [movies, setMovies] = useState([]);
 
-    // A snippet of code which runs based on a specific condition/variable
-    useEffect(() => {
+	// A snippet of code which runs based on a specific condition/variable
+	useEffect(() => {
+		async function fetchData() {
+			const request = await axios.get(fetchURL);
+			setMovies(request.data.results);
+			return request;
+		}
+		fetchData();
+	}, [fetchURL]);
+	// you must include fetchURL in the bracket, otherwise it will not run. it's a dependency. it's saying, "hey, this variable is being pulled from outside the block, so you need to include it in the bracket so that it knows to run every time the variable changes."
+	// if [] (bracket is empty), run once when the row loads, and don't run again. but if there isa  variable in the bracket, it will run every time the variable changes. it's dependent on that.
+	return (
+		<div className="row">
+			<h2>{title}</h2>
 
-        async function fetchData() {
-            const request = await axios.get(fetchURL);
-            setMovies(request.data.results);
-            return request;
-        }
-        fetchData();
-    }, [fetchURL]);
-    // you must include fetchURL in the bracket, otherwise it will not run. it's a dependency. it's saying, "hey, this variable is being pulled from outside the block, so you need to include it in the bracket so that it knows to run every time the variable changes."
-    // if [] (bracket is empty), run once when the row loads, and don't run again. but if there isa  variable in the bracket, it will run every time the variable changes. it's dependent on that.
-    console.table(movies);
+			<div className="row__posters">
+				{/* several row__poster(s) */}
 
-
-    return (
-    <div className="row">
-        <h2>{ title }</h2>
-
-        <div className="row__posters">
-            { /* several row__poster(s) */ }
-
-            {movies.map(movie => (
-                <img 
-                    className="row_poster"
-                    src={`${base_url}${movie.poster_path}`}
-                    alt={movie.name}
-                />
-                // needed base_url because the poster_path is only the end of the url. it's not the full url.
-            ))}
-        </div>
-    </div>
-    )
+				{movies.map((movie) => (
+					<img
+						key={movie.id} // this is for optimization. it's saying, "hey, if you already have this movie, don't re-render it."
+						className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+						// if isLargeRow is true, then add the class row__posterLarge. if it's false, then don't add it.
+						src={`${base_url}${
+							isLargeRow ? movie.poster_path : movie.backdrop_path
+						}`}
+						alt={movie.name}
+					/>
+					// needed base_url because the poster_path is only the end of the url. it's not the full url.
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default Row;
